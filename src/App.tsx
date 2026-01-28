47cd9c43e60bb8972687c7dd086faae3fa13c952
DeSadGuy <devinchen1006@gmail.com>
2026-01-28 14:25:13 +0100

    Dashboard, inlog, graph

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
+import Inlog from "./pages/Inlog";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Inlog />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </BrowserRouter>
---
+++ src/layouts/InlogLayout.tsx
@@ -0,0 +1,5 @@
+const InlogLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return <div className="">{children}</div>;
+};
+
+export default InlogLayout;
